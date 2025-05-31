086771eb653afc0bb7557b752153f271de7d3100
Osama Ayub <ayubosama416@gmail.com>
2025-05-31 23:02:28 +0500

    implemented ESlint & prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono as GeistMono } from "next/font/google";
 import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = GeistMono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/react-in-jsx-scope */
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-4 row-start-2 items-center mt-6">
-      <h1 className="text-3xl text-center text-amber-800 font-black">Hello Next Js</h1>
+      <h1 className="text-3xl text-purple-700">Next js Course</h1>
     </main>
   );
 }
