dee6b01caada5fe53ac4e6ac73c8c88a72a6e109
Osama Ayub <ayubosama416@gmail.com>
2025-06-01 00:16:12 +0500

    Tailwind configuration added for v4 version

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex flex-col gap-4 row-start-2 items-center mt-6">
-      <h1 className="text-3xl text-purple-700">Next js Course</h1>
+      <h1 className="text-3xl text-light-500">Next js Course</h1>
     </main>
   );
 }
