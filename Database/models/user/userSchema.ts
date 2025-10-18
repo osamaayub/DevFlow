944d351b7f4103baa352ce79d647498679c912e8
osamayub <osama.ayubwebdev@gmail.com>
2025-10-18 19:58:52 +0500

    created user schema & model

---
+++ Database/models/user/userSchema.ts
@@ -0,0 +1,48 @@
+import { Schema } from "mongoose"
+
+export interface IUser {
+  name: string
+  username: string
+  email: string
+  bio?: string
+  image: string
+  location?: string
+  portfolio?: string
+  reputation?: number
+}
+
+export const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    bio: {
+      type: String
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String
+    },
+    portfolio: {
+      type: String
+    },
+    reputation: {
+      type: Number,
+      default: 0
+    }
+  },
+  { timestamps: true }
+)
