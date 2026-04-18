e09203e4ae005bc328efc7237941c87d41ad5677
Osama Ayub <ayubosama416@gmail.com>
2026-04-18 12:07:50 +0500

    create Question schema & model

--- database/models/Question/Question.schema.ts
+++ database/models/Question/Question.schema.ts
@@ -1,16 +1,48 @@
-import { Types } from "mongoose";
-import { Schema } from "mongoose";
+import { Schema, Types } from "mongoose"
 
-
-
-export interface IQuestion{
-   
+export interface IQuestion {
+  title: string
+  content: string
+  tags: Types.ObjectId[]
+  views: Number
+  upvotes: Number
+  downvotes: Number
+  answers: Number
+  author: Types.ObjectId
 }
 
-
-
-export const QuestionsSchema= new Schema({
-
-    
-
-},{timestamps:true});
+export const QuestionsSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    views: {
+      type: Number,
+      default: 0
+    },
+    upvotes: {
+      type: Number,
+      default: 0
+    },
+    downvotes: {
+      type: Number,
+      default: 0
+    },
+    answers: {
+      type: Number,
+      default: 0
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required:true
+    }
+  },
+  { timestamps: true }
+)
