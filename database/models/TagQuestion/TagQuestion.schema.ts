82a14d476da7ef745e3115426c1238c91f491976
osamaayub <ayubosama416@gmail.com>
2026-05-02 22:55:51 +0500

    changed the schema field from tagid to tag

--- database/models/TagQuestion/TagQuestion.schema.ts
+++ database/models/TagQuestion/TagQuestion.schema.ts
@@ -3,12 +3,12 @@ import { Schema, Types } from "mongoose";
 
 export interface ITagQuestion {
     question: Types.ObjectId,
-    tagId: Types.ObjectId
+    tag: Types.ObjectId
 }
 
 export const TagQuestionSchema = new Schema({
     question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
-    tagId: { type: Schema.Types.ObjectId, ref: "Tag", required: true }
+    tag: { type: Schema.Types.ObjectId, ref: "Tag", required: true }
 }, {
     timestamps: true
 })
