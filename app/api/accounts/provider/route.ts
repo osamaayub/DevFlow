fc1909ad76cad2d316566d8ca1b2b9fb70f1d708
osamaayub <ayubosama416@gmail.com>
2026-07-12 15:22:12 +0500

    implement route for account using provider

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,32 @@
+
+import { HandleError } from "@/lib/handlers";
+import { NextResponse, NextRequest } from "next/server";
+import { Account } from "@/database/models";
+import { dbConnect, ValidationError } from "@/lib";
+
+export async function POST(request: NextRequest) {
+    try {
+        await dbConnect();
+        const body = await request.json();
+        const { providerAccountId } = body;
+        if (!providerAccountId || typeof providerAccountId !== "string") {
+           throw new ValidationError({ providerAccountId: ["Provider Account ID is required."] });
+        }
+        const account = await Account.findOne({ providerAccountId });
+        if (!account) {
+            return NextResponse.json({
+                message: "Account not found",
+                success: false,
+            }, { status: 404 });
+        }
+        return NextResponse.json({
+            message: "Account found",
+            success: true,
+            data: account
+        }, { status: 200 });
+    }
+    catch (error: unknown) {
+        const errorMessage = error instanceof Error ? error.message : "Something went wrong";
+        return HandleError(errorMessage) as unknown as ApiErrorResponse;
+    }
+}
