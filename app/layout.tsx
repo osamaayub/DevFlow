adf028cc9da5af645400c9477b96907b09f1b647
osamaayub <ayubosama416@gmail.com>
2026-08-11 03:00:47 -0700

    fixed the issues regarding linting

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { auth } from "@/auth";
+
 import RootProviders from "@/components/RootProviders";
 
 const inter = Inter({
