7583ca8c84697c45843d2bfa6fc1d5521185b80a
osamaayub <ayubosama416@gmail.com>
2026-08-15 05:37:11 -0700

    fixed eslint errors

--- lib/index.ts
+++ lib/index.ts
@@ -4,4 +4,14 @@ export * from "./url"
 export * from "./validation"
 export * from "./handlers"
 export * from "./utils"
-export * from "./http-error"
+export * from "./api"
+export * from "./logger"
+
+// ✅ Add this consolidated export to satisfy `import { api } from "@/lib"` in auth.ts
+import { usersApi, accountsApi, authApi } from "./api"
+
+export const api = {
+  users: usersApi,
+  accounts: accountsApi,
+  auth: authApi
+}
