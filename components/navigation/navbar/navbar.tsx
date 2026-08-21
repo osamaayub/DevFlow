32c4d7b02da4348f0ce1d48cb2377cabf0a7a880
osamaayub <ayubosama416@gmail.com>
2026-08-21 11:09:18 -0700

    fixed avatar not showing issue in navbar

--- components/navigation/navbar/navbar.tsx
+++ components/navigation/navbar/navbar.tsx
@@ -1,28 +1,34 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
 
-import { Theme } from "../Theme";
-import MobileNavigation from "./MobileNavigation";
+import { auth } from "@/auth"
+import UserAvatar from "@/components/userAvatar"
 
-export function Navbar() {
+import { Theme } from "../Theme"
+import MobileNavigation from "./MobileNavigation"
+
+export async function Navbar() {
+  const session = await auth()
+  const user = session?.user
+
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 shadow-light-300 dark:shadow-none sm:px-12 ">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
-        <Image
-          src="/images/site-logo.svg"
-          alt=" DevFlow logo"
-          width={23}
-          height={23}
-        />
-        <p className=" h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+        <Image src="/images/site-logo.svg" alt="DevFlow logo" width={23} height={23} />
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
           Dev <span className="text-primary-500">Flow</span>
         </p>
       </Link>
-       Search bar
-      <div className=" flex-between gap-5">
+
+      <div className="flex-between gap-5">
         <Theme />
+
+        {user?.id && user.name && (
+          <UserAvatar id={user.id} name={user.name} image={user.image} className="size-9" />
+        )}
+
         <MobileNavigation />
       </div>
     </nav>
-  );
+  )
 }
