48902948ec6a2245551f01e560e0f27d976a2229
osamaayub <ayubosama416@gmail.com>
2026-08-27 04:12:11 -0700

    implement tag server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,5 +1,23 @@
-const Tags = () => {
-  return <div>page</div>;
+import {getTags} from "@/lib/actions"
+
+
+const Tags = async () => {
+   const {success,data,error}=await  getTags({
+     page:1,
+     pageSize:10,
+     query:""
+   })
+  const {tags}=data || {};
+  return(
+    <div className='flex flex-col items-center gap-2'>
+      {tags?.map((tag:Tag)=>(
+        <div className='' key={tag._id}>
+          <p className=''>{tag.name}</p>
+          <p>{tag.questions}</p>
+        </div>
+      ))}
+    </div>
+  )
 };
 
 export default Tags;
